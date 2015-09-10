0a4ec6a108f890615891c2d89a5822b3df14fa3f
Dillon <dwd002@lvc.edu>
2015-09-10 18:38:59 -0400

    Added the try this for 9/11 and also finsihed exercise 10 for the weekly project

---
+++ Eclipse C++ WorkSpace/TryThis_4.4.2.1/source.cpp
@@ -0,0 +1,20 @@
+/*
+ * source.cpp
+ *
+ *  Created on: Sep 10, 2015
+ *      Author: dillon
+ */
+#include <iostream>
+using namespace std;
+
+int main(){
+	int i = 97;
+	while(i < 122){
+		char out = i;
+		cout << out;
+		i++;
+	}
+}
+
+
+
