25821a5d2d296da889712687069d0b8c6905db5a
Dillon <dwd002@lvc.edu>
2015-10-06 00:13:25 -0400

    added exercise 3 and 4 from chapter 7

--- Projects/Project2b/source.cpp
+++ Projects/Project2b/source.cpp
@@ -12,9 +12,28 @@ using namespace std;
 
 //this function will create a token from user input
 int main() {
+
 	Token_Stream ts;
-	Expression(ts);
+
+	try {
+		double val = 0.0;
+		while (cin) {
+			Token t = ts.get();
+			if (t.kind == 'q')
+				break;
+			else if (t.kind == ';')
+				cout << "=" << val << "\n";
+				//keep_window_open("~~");
+			else {
+				ts.putback(t);
+				val = Expression(ts);
+			}
+		}
+	} catch (exception& e) {
+		cout << e.what();
+	} catch (...) {
+		cout << "something bad happened take cover hide its ww3";
+	}
+
 }
-
-
 
